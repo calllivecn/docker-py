4ac7a4bc2cd2ea93dc21a630549a06dcb70e1b49
calllivecn <calllivecn@outlook.com>
2018-08-20 16:44:39 +0800

    新文件：   .gitignore 新文件：   dockerapi-test.py

---
+++ dockerapi-test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#coding=utf-8
+# date 2018-07-20 15:01:37
+# author calllivecn <c-all@qq.com>
+
+URL='tcp://192.168.57.101:2375'
+
+import docker
+import pprint
+
+client = docker.DockerClient(base_url=URL)
+if not client.ping():
+    print("not ping...")
+    exit(1)
+
+
+
+def run_test():
+    detach = client.containers.run("alpine","ping www.baidu.com",detach=True)
+
+    for line in detach.logs(stream=True):
+        print(line.strip())
+
+
+def stats():
+    con_det = client.containers.list()
+    print("在run的容器:",con_det)
+    for stat in con_det:
+        stat_info = stat.stats(stream=False)
+        pprint.pprint(stat_info)
+
+
+#client.containers.run("alpine","ping www.baidu.com",detach=True)
+
+stats()
